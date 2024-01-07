0e0fbe7cc4bbc339ea7421126d9c4fd4cef7f3be
umitsu-toshisato <75233427+umitsu-toshisato@users.noreply.github.com>
2024-01-07 16:31:13 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private float value = 3.1415926f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(value);
+    }
+}
